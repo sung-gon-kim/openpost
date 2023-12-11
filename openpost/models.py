0b43b2ec353c4a7a9c86a9cf92be3906b4746cca
Sung Gon Kim <sunggon82.kim@lge.com>
2023-12-11 17:27:54 +0900

    Add Note model

--- openpost/models.py
+++ openpost/models.py
@@ -10,3 +10,11 @@ class Column(models.Model):
     def __str__(self):
         return self.title
 
+class Note(models.Model):
+    column = models.ForeignKey("Column", on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    created = timezone.now()
+
+    def __str__(self):
+        return self.title
--- openpost/tests.py
+++ openpost/tests.py
@@ -1,16 +1,39 @@
 from .models import Column
+from .models import Note
 from django.db.utils import IntegrityError
 from django.test import TestCase
 
 
 class ColumnTests(TestCase):
+    def setUp(self):
+        self.column = Column.objects.create(title="column", description="description")
+
     def test_create(self):
-        o = Column.objects.create(title="aaa", description="bbb")
-        self.assertEqual("aaa", str(o))
-        self.assertEqual("aaa", o.title)
-        self.assertEqual("bbb", o.description)
+        self.assertEqual("column", str(self.column))
+        self.assertEqual("column", self.column.title)
+        self.assertEqual("description", self.column.description)
 
     def test_title_is_primary_key(self):
         with self.assertRaises(IntegrityError):
-            Column.objects.create(title="aaa")
-            Column.objects.create(title="aaa")
+            Column.objects.create(title="column", description="description")
+
+class NoteTests(TestCase):
+    def setUp(self):
+        self.column = Column.objects.create(title="column", description="description")
+        self.note = Note.objects.create(column=self.column, title="note", content="content")
+
+    def test_create(self):
+        self.assertEqual("note", str(self.note))
+        self.assertEqual("note", self.note.title)
+        self.assertEqual("content", self.note.content)
+
+    def test_uid_is_primary_key(self):
+        with self.assertRaises(IntegrityError):
+            Note.objects.create(column=self.column, id=1, title="note", content="content")
+
+    def test_title_is_not_unique(self):
+        Note.objects.create(column=self.column, title="note", content="content")
+
+    def test_cascade_delete(self):
+        self.column.delete()
+        self.assertEqual(0, Note.objects.count())
