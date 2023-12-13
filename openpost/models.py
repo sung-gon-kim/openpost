ff96af1333e6ebe6b5e3864f01dc51be943a1948
Sung Gon Kim <sunggon82.kim@lge.com>
2023-12-13 17:32:11 +0900

    Replace long CharFields with TextFields

--- openpost/models.py
+++ openpost/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-
 SHORT_CHAR_LENGTH = 128
-LONG_CHAR_LENGTH = 512
 
 class Column(models.Model):
     title = models.CharField(max_length=SHORT_CHAR_LENGTH)
-    description = models.CharField(max_length=LONG_CHAR_LENGTH)
+    description = models.TextField()
     created = timezone.now()
 
     def __str__(self):
@@ -16,7 +14,7 @@ class Column(models.Model):
 class Note(models.Model):
     column = models.ForeignKey("Column", on_delete=models.CASCADE)
     title = models.CharField(max_length=SHORT_CHAR_LENGTH)
-    content = models.CharField(max_length=LONG_CHAR_LENGTH)
+    content = models.TextField()
     created = timezone.now()
 
     def __str__(self):
@@ -24,7 +22,7 @@ class Note(models.Model):
 
 class Comment(models.Model):
     note = models.ForeignKey("Note", on_delete=models.CASCADE)
-    content = models.CharField(max_length=LONG_CHAR_LENGTH)
+    content = models.TextField()
     created = timezone.now()
 
     def __str__(self):
