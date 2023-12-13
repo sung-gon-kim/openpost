6e69d297d4dd0bf6029889764d47fd7a38ce13db
Sung Gon Kim <sunggon82.kim@lge.com>
2023-12-13 17:26:32 +0900

    Update index view

--- openpost/admin.py
+++ openpost/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from .models import Column
+from .models import Note
+from .models import Comment
 
 admin.site.register(Column)
+admin.site.register(Note)
+admin.site.register(Comment)
--- openpost/views.py
+++ openpost/views.py
@@ -1,5 +1,7 @@
+from .models import Column
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "openpost/index.html")
+    context = { "columns": Column.objects.all() }
+    return render(request, "openpost/index.html", context=context)
