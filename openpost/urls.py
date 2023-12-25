4161b783b9afe25fe21c6ba84e7319cf0197d05c
Sung Gon Kim <sunggon82.kim@lge.com>
2023-12-26 00:23:03 +0900

    Fix an issue that initial page becomes empty

--- openpost/urls.py
+++ openpost/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "openpost"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("post/", views.post, name="post"),
 ]
--- openpost/views.py
+++ openpost/views.py
@@ -1,7 +1,19 @@
 from .models import Post
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 def index(request):
-    context = { "data": Post.objects.all() }
+    context = {
+        "notices": Post.objects.filter(section="공지사항"),
+        "questions": Post.objects.filter(section="질문"),
+        "opinions": Post.objects.filter(section="의견"),
+    }
     return render(request, "openpost/index.html", context=context)
+
+def post(request):
+    o = Post.objects.create(section=request.POST["section"],
+                            subject=request.POST["subject"],
+                            content=request.POST["content"])
+    o.save()
+    return redirect("openpost:index")
