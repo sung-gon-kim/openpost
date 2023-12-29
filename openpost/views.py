60025c902ea0ced7d7ba171819ca939ee41af2eb
Sung Gon Kim <sunggon82.kim@lge.com>
2023-12-29 10:47:28 +0900

    Add post edit feature

--- openpost/views.py
+++ openpost/views.py
@@ -20,14 +20,31 @@ def add_post(request):
         post.save()
     return redirect("openpost:index")
 
+def edit_post(request, id):
+    if id is not None:
+        context = {
+            "notices": Post.objects.filter(section="공지사항"),
+            "questions": Post.objects.filter(section="질문"),
+            "opinions": Post.objects.filter(section="의견"),
+            "post": Post.objects.get(id=id),
+        }
+        return render(request, "openpost/index.html", context=context)
+    return redirect("openpost:index")
+
+def update_post(request):
+    if "id" in request.POST and "subject" in request.POST and "content" in request.POST:
+        Post.objects.filter(id=request.POST["id"]).update(subject=request.POST["subject"],
+                                                          content=request.POST["content"])
+    return redirect("openpost:index")
+
 def remove_post(request, id):
     if id is not None:
         Post.objects.filter(id=id).delete()
     return redirect("openpost:index")
 
 def add_comment(request):
-    if "post" in request.POST and "content" in request.POST:
-        post = Post.objects.get(id=request.POST["post"])
+    if "id" in request.POST and "content" in request.POST:
+        post = Post.objects.get(id=request.POST["id"])
         comment = Comment.objects.create(post=post,
                                          content=request.POST["content"])
         comment.save()
